85a7009fcf857104cf8e3336bf50d466f88c591b
git-marcopitra <git-marcopitra@github.com>
2020-05-22 03:49:45 +0100

    ADD: Data.js to divide tasks NEW: Functional Flex Form

---
+++ data.js
@@ -0,0 +1,61 @@
+var types = [
+    'text',
+    'number',
+    'password',
+    'radio',
+    'checkbox',
+    'select',
+    'option',
+    'text'
+];
+
+var formy =  [
+    {
+        idForm: 1,
+        formCode: "signin",
+        formName: "Sign In"
+    },
+    {
+        idForm: 2,
+        formCode: "signup",
+        formName: "Sign Up"
+    }
+];
+
+var fields = [
+    {
+        id: 0,
+        form: 'signin',
+        name: 'username',
+        type: 'text',
+        placeholder: 'Nome de utilizado'
+    },
+    {
+        id: 1,
+        form: 'signin',
+        name: 'password',
+        type: 'password',
+        placeholder: 'Palavra-passe'
+    },
+    {
+        id: 2,
+        form: 'signup',
+        name: 'fullname',
+        type: 'text',
+        placeholder: 'Nome Completo'
+    },
+    {
+        id: 3,
+        form: 'signup',
+        name: 'username',
+        type: 'text',
+        placeholder: 'Nome de utilizado'
+    },
+    {
+        id: 4,
+        form: 'signup',
+        name: 'password',
+        type: 'password',
+        placeholder: 'Palavra-passe'
+    }
+];
--- script.js
+++ script.js
@@ -1,119 +1,64 @@
 console.log("Script Works");
 
 var forms = document.getElementById('forms');
-var tbInputs = document.querySelectorAll('.dados');
-
-if (forms) 
-    forms.addEventListener('change', tarefa);
-
-
-tbInputs.forEach(el => {
-    el.addEventListener('blur', updateForm);
-});
-
-var formy =  [
-    {
-        idForm: 1,
-        formCode: "signin",
-        formName: "Sign In"
-    },
-    {
-        idForm: 2,
-        formCode: "signup",
-        formName: "Sign Up"
-    }
-]
-
-var types = [
-    'text',
-    'number',
-    'password',
-    'radio',
-    'checkbox',
-    'select',
-    'option',
-    'text'
-]
-
-var fields = [
-    {
-        form: 'signin',
-        field: 'si-username',
-        type: 'text',
-        placeholder: 'Nome de utilizado'
-    },
-    {
-        form: 'signin',
-        field: 'si-password',
-        type: 'password',
-        placeholder: 'Palavra-passe'
-    },
-    {
-        form: 'signup',
-        field: 'su-fullname',
-        type: 'text',
-        placeholder: 'Nome Completo'
-    },
-    {
-        form: 'signup',
-        field: 'su-username',
-        type: 'text',
-        placeholder: 'Nome de utilizado'
-    },
-    {
-        form: 'signup',
-        field: 'su-password',
-        type: 'password',
-        placeholder: 'Palavra-passe'
-    }
-];
-
 var tbody = document.getElementById('corpo');
 var form = document.getElementById('container');
 
-function tarefa(){
+forms.addEventListener('change', renderView);
+
+function renderView(){
     var tr;     
-    let formies;
     let typies;
     
     tbody.innerHTML = "";
     form.innerHTML = `
         <legend>Pre-visualização</legend>
         <form name="${forms.value}">
-    ` ;
+    `;
 
-    fields.map(row => {
-        formies = '';
+    fields.forEach(row => {
         typies = '';
         if (row.form == forms.value){ 
             tr = `
+                <td> ${row.form} </td>
+                <td> <input id="name-${row.id}" class="iTable" id="" value="${row.name}"> </td>
                 <td> 
-                    <select value="${row.form}">
-                        <option disabled value=""> Selecione o formulário </option>
-                        ${formy.forEach(f => formies += `<option ${forms.value === f.formCode ? 'selected':''} value="${f.formCode}">${f.formName}</option>`)}
-                        ${formies}
-                    </select>
-                </td>
-                <td> <input class="dados" value="${row.field}"> </td>
-                <td> 
-                    <select value="${row.type}">
+                    <select id="type-${row.id}" class="iTable">
                         <option disabled value=""> Selecione o tipo </option>
                         ${types.forEach(f => typies += `<option ${f === row.type ? 'selected':''} value="${f}">${f}</option>`)}
                         ${typies}
                     </select>
                 </td>
-                <td> <input id="p-${row.field}" class="dados" value="${row.placeholder}"> </td>
-                <td><button onclick="tarefa()"> OK </button></td>
+                <td> <input id="placeholder-${row.id}" class="iTable" value="${row.placeholder}"> </td>
+                <td> <button onclick="tarefa()"> Refresh </button> </td>
             `;
 
             tbody.innerHTML += `<tr> ${tr} </tr>`;
 
             form.innerHTML += `
                 <br /><label> ${row.placeholder} <br />
-                <input type="${row.type}" name="${row.field}" placeholder="${row.placeholder}">
+                <input type="${row.type}" name="${row.name}" placeholder="${row.placeholder}">
                 </label>
             `;
         }
     });
-    form.innerHTML += "</form>"
+
+    form.innerHTML += "</form>";
+    getInputs();
+}
+
+function refresh($id, $value) {
+    let arrow = $id.split('-');
+    fields[arrow[1]][arrow[0]] = $value;
+    renderView();
+}
+
+function getInputs() {
+    let tbInputs = document.querySelectorAll('.iTable');
+
+    tbInputs.forEach(el => {
+        el.addEventListener('change', event => {
+            refresh(event.target.id, event.target.value)
+        });
+    });
 }
